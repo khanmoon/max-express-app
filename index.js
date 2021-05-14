caf9dc87d27440c62af5492b40f6da6ed178c655
Mansoor Naseer <mansoor.naseer@backcountry.com>
2021-05-14 17:27:19 +0500

    extracting controller to routers.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 
-app.use(adminRouter);
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button>Submit</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button>Submit</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-  res.redirect("/add-product");
+  res.redirect("/admin/add-product");
 });
 module.exports = router;
