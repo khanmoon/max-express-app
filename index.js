6d738dc0b41e62f7015c6a98def1dfa206fa7142
Mansoor Naseer <mansoor.naseer@backcountry.com>
2021-05-14 14:30:44 +0500

    body-parser added.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 const adminRouter = require("./routes/admin");
 
 app.use(adminRouter);
