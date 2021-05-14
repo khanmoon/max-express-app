99a3a8a6a60808ebc84a27465ee4ad332bb7dea1
Mansoor Naseer <mansoor.naseer@backcountry.com>
2021-05-14 23:54:05 +0500

    templating engin wireup

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
