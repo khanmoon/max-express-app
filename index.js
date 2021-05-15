179748665cc2170feed231e4f1960478f5300665
Mansoor Naseer <mansoor.naseer@backcountry.com>
2021-05-15 14:43:08 +0500

    adding handle bars.

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const rootDir = require("./util/path");
 const path = require("path");
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
 app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,6 +18,6 @@ app.use("/admin", adminRouter.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).render("404.pug");
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 app.listen(3000);
