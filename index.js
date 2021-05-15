9a0c9545c1ce2ed608c992ce1e2c9f3e8de3f6b6
Mansoor Naseer <mansoor.naseer@backcountry.com>
2021-05-15 15:14:58 +0500

    adding ejs

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const rootDir = require("./util/path");
 const path = require("path");
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
-const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine("hbs", expressHbs());
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
